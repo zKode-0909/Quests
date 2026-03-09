d25129847565af35b3eec0151625f4f269d7b095
Zachary Kociban <zkociban@gmail.com>
2026-03-09 17:08:07 -0400

    moving to PC

--- Assets/Scripts/SimPlayer/SimPlayer.cs
+++ Assets/Scripts/SimPlayer/SimPlayer.cs
@@ -1,7 +1,28 @@
 using UnityEngine;
 
-public class SimPlayer : MonoBehaviour
+public class SimPlayer : MonoBehaviour,IEntity,IDamageable,IInteractor,IInteractable
 {
+
+    public GameObject GameObject => gameObject;
+
+    public int EntityRuntimeID => entityRuntimeID;
+
+    public int EntityLevel => entityLevel;
+
+    int entityRuntimeID;
+
+    int entityLevel;
+
+    public void HandleInteract(IInteractor interactor)
+    {
+        Debug.Log($"{interactor} has interacted with me.");
+    }
+
+    public void TakeDamage(float damage, int damagerRuntimeID)
+    {
+        Debug.Log($"I have taken {damage} damage {damagerRuntimeID}");
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
